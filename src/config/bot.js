c49c9842f8b0c20e0155440e4aa4bb3e5580e79f
AlievMR <magomedrasul-x@outlook.com>
2026-09-22 11:07:16 +0400

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,7 +93,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#d3b300",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
